d8eecda7eeaf3a1265db61b25a661a73e4c4fbfe
trtobeha <trtobeha@gmail.com>
2024-04-30 01:34:48 +0300

    Edit dependencies type

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -18,7 +18,7 @@ router = APIRouter()
 @router.get(
     '/',
     response_model=list,
-    dependencies=[Depends(current_superuser)],
+    dependencies=(Depends(current_superuser),),
 )
 async def get_report(
     session: AsyncSession = Depends(get_async_session),
